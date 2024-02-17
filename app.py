2bfc71f37c82da6c38ae6af156227f77f9ff22f9
So-Ebrahimi <sobhan101@proton.me>
2024-02-17 20:11:20 +0330

    start update db def

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask 
+import pandas as pd
+import sqlite3
+
+
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/test")
 def main_page() : 
-    return "hello"
+    """ this is test page """
+    return "test"
+
+
+def update_db(path)  :
+    """update database from xlsx file  """
+    # conn = sqlite3.connect(host='', port=0, timeout=None, source_address=None)
+    # curl = conn.crusor()
+    # curl.execute(sql)
+    df = pd.read_excel(path)
+    # df headers = index , name , phone 
+    for  name , phone in df.iterrows() : 
+        print( name , phone)
 
 
 if __name__ ==  "__main__":
-    app.run()
+    update_db("data.xlsx")
+    app.run(host="0.0.0.0", port="12345",debug=True)
+
+
+
