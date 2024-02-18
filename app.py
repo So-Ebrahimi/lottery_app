3c9834c76f0dbdac85e33927e17baa9f2809eaef
So-Ebrahimi <sobhan101@proton.me>
2024-02-18 22:57:38 +0330

    complate db defs

--- app.py
+++ app.py
@@ -18,10 +18,44 @@ def update_db(path)  :
     curl = conn.cursor()
     #upload new data to db 
     df = pd.read_excel(path)
-    df.to_sql(name='PEOPLE',con=conn,if_exists='replace',index=True)
+    df.to_sql(name='PEOPLE',con=conn,if_exists='replace',index=False)
     conn.commit()
     conn.close()
     print("Db  updated suxcesfully")
+
+def Find_prize_slice(table_name , count_winner ) :
+    '''this function  find winner by count of prize in table '''
+    conn = sqlite3.connect("sql.db")
+    curl = conn.cursor()
+    curl.execute("SELECT Count() FROM %s" % table_name )
+    numberOfRows = curl.fetchone()[0]
+    prize_slice = numberOfRows /  count_winner
+    prize_slice = int(prize_slice)
+    return (numberOfRows, prize_slice)
+
+
+def find_winner_from_db(table_name , id) :
+    '''this function  find winner by count of prize in table '''
+    conn = sqlite3.connect("sql.db")
+    curl = conn.cursor()
+    rowsQuery = f"SELECT * FROM {table_name} where ROW='{id}'"
+    print(rowsQuery)
+    curl.execute(rowsQuery)
+    rows = curl.fetchall()
+    winner_name , winer_phone = rows[0][1] , rows[0][2]
+    return (winner_name , winer_phone)
+
+
+
+def Find_winner_id( table_name, numberOfRows , prize_slice ,winner_id ):
+    """this function find winner id and  return it  """
+    range_win = int(numberOfRows / prize_slice ) 
+    winners_list = []
+    start = 0 
+    for winer  in range(range_win):
+        id =  start + winner_id 
+        winner_name , winer_phone = find_winner_from_db(table_name , id) 
+        print("winner_name , winer_phone")
 
 
 
