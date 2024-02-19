2755ed0c668cf56006d0e027608ce5cdab377286
So-Ebrahimi <sobhan101@proton.me>
2024-02-19 21:26:28 +0330

    first step of login page

--- app.py
+++ app.py
@@ -1,15 +1,84 @@
-from flask import Flask 
+from flask import Flask , Response, redirect, url_for, request, session, abort
+from flask_login import LoginManager, UserMixin, \
+                                login_required, login_user, logout_user 
 import pandas as pd
 import sqlite3
 
 
 app = Flask(__name__)
 
+# config
+app.config.update(
+    SECRET_KEY = 'secret_xxx'
+)
 
-@app.route("/test")
-def main_page() : 
-    """ this is test page """
-    return "test"
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        
+    def __repr__(self):
+        return "%d" % (self.id)
+
+# create some users with ids 1 to 20       
+user = User(0)
+
+
+# some protected url
+@app.route('/')
+@login_required
+def home():
+    return Response("Hello World!")
+
+# somewhere to login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']        
+        if username == "sobhan" and password == "1234" :
+            login_user(user)
+            return redirect("/")
+        else:
+            return abort(401)
+    else:
+        return Response('''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        ''')
+
+
+# somewhere to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>Logged out</p>')
+
+
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+    
+    
+# callback to reload the user object        
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+
+
+
 
 
 def update_db(path)  :
@@ -62,6 +131,3 @@ def Find_winner_id( table_name, numberOfRows , prize_slice ,winner_id ):
 if __name__ ==  "__main__":
     update_db("data.xlsx")
     app.run(host="0.0.0.0", port="12345",debug=True)
-
-
-
